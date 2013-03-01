f869f909e281b7dbc9199d804d5b2c605b5b6400
Matthew K. Fu JuYuan <matthewfu@gmail.com>
2013-03-01 13:50:15 +0800

    install script

--- install_server.rb
+++ install_server.rb
@@ -3,18 +3,22 @@ include FileUtils
 
 $script_root = pwd()
 if %x[uname].split("\n").first == 'Linux'
-  cd "#{$script_root}/mongo"  
-    %x[sudo aptitude install scons build-essential]
-    %x[sudo aptitude install git-core build-essential scons]
-    puts 'This usually takes time'
-    %x[scons all]
-    %x[sudo scons --prefix=/usr/bin/mongo install]
+  cd "#{$script_root}/mongo/bin"  
+  %x[sudo cp * /usr/sbin/]
+  %x[sudo mkdir /data/db/]
+  %x[sudo mkdir /data/db/]
+  cd "#{$script_root}"  
+  %x[sudo cp inits/mongo/mongodb /etc/init.d/]
+  %x[sudo cp confs/mongo/mongodb.conf /etc/]
 
 #redis
   cd "#{$script_root}/redis"
   %x[make]
   %x[sudo cp src/redis-server /usr/local/bin/]
   %x[sudo cp src/redis-cli /usr/local/bin/]
+  cd "#{$script_root}"  
+  %x[sudo cp inits/redis/redis_6379 /etc/init.d/]
+  %x[sudo cp confs/redis/6379.conf /etc/redis/]
   
 end # of linux
 
