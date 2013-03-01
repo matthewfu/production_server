1bb75ee3dcfcba60a66659a4f93b9cb3cb8f0c31
Matthew K. Fu JuYuan <matthewfu@gmail.com>
2013-03-01 20:38:18 +0800

    add nginx conf

--- install_server.rb
+++ install_server.rb
@@ -2,40 +2,47 @@ require 'fileutils'
 include FileUtils
 
 $script_root = pwd()
+$gem_home = ENV["GEM_HOME"]
+$user_home = ENV["HOME"]
 if %x[uname].split("\n").first == 'Linux'
 
 #mongo
   cd "#{$script_root}/mongo/bin"  
-  %x[sudo cp * /usr/sbin/]
-  %x[sudo mkdir -p /data/db/]
+  `sudo cp * /usr/sbin/`
+  `sudo mkdir -p /data/db/`
   cd "#{$script_root}"  
-  %x[sudo cp inits/mongo/mongodb /etc/init.d/]
-  %x[sudo cp confs/mongo/mongodb.conf /etc/]
-  %x[sudo update-rc.d mongodb defaults]
+  `sudo cp inits/mongo/mongodb /etc/init.d/`
+  `sudo cp confs/mongo/mongodb.conf /etc/`
+  `sudo update-rc.d mongodb defaults`
 
 #redis
   cd "#{$script_root}/redis"
-  %x[make]
-  %x[sudo cp src/redis-server /usr/local/bin/]
-  %x[sudo cp src/redis-cli /usr/local/bin/]
+  `make`
+  `sudo cp src/redis-server /usr/local/bin/`
+  `sudo cp src/redis-cli /usr/local/bin/`
   cd "#{$script_root}"  
-  %x[sudo cp inits/redis/redis_6379 /etc/init.d/]
-  %x[sudo mkdir -p /etc/redis/]
-  %x[sudo cp confs/redis/6379.conf /etc/redis/]
-  %x[sudo update-rc.d redis_6379 defaults]
+  `sudo mkdir -p /var/redis/6379`
+  `sudo cp inits/redis/redis_6379 /etc/init.d/`
+  `sudo mkdir -p /etc/redis/`
+  `sudo cp confs/redis/6379.conf /etc/redis/`
+  `sudo update-rc.d redis_6379 defaults`
 #pcre
   cd "#{$script_root}/pcre"
-  %x[tar -xvjpf pcre-8.32.tar.bz2]
+  `tar -xvjpf pcre-8.32.tar.bz2`
   cd  "pcre-8.32"
-  %x[./configure] 
-  %x[make] 
-  %x[sudo make install] 
-  %x[sudo ldconfig]
+  `./configure`
+  `make`
+  `sudo make install`
+  `sudo ldconfig`
 
 #nginx
+  `gem install passenger`
   cd "#{$script_root}/nginx"
-  puts "END OF SCRIPT"
-
+  `./auto/configure --add-module=#{$script_root}/plugins/nginx-gridfs.git --add-module=#{$gem_home}/gems/passenger-3.0.19/ext/nginx --prefix=#{$user_home}/nginx`
+  `sudo cp #{$script_root}/inits/nginx/nginx /etc/init.d/`
+  `sudo cp #{$script_root}/conf/nginx/nginx.conf #{$user_home}/nginx/conf/`
+    puts 'Please edit nginc conf for detial'
+  `sudo update-rc.d nginx defaults`
 #varnish
 
 end # of linux
